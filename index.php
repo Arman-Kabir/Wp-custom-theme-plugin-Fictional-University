2532c88d77d1a41fd3f49ec35aa557032a2f5b43
Arman-Kabir <armankabir8@gmail.com>
2023-03-28 16:23:46 +0600

    functions

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-this is our completely amazing customized theme
+<?php
+function greet($name, $color)
+{
+    echo " <p>Hi, my name is $name  and my fav color is $color  ";
+}
+
+greet('John', 'blue');
+greet('Jane', 'green');
+
+?>
+
+
+<h1><?php bloginfo('name'); ?>  </h1>
+
+<p><?php bloginfo('description');  ?>  </p>
